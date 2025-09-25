5e4e3d9304f8f182da97e62c95aec0dc95c68984
Laurenz Kremeyer <laurenz.kremeyer@mail.mcgill.ca>
2025-09-25 11:54:56 -0400

    auto download test assets

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,13 @@
 
 All notable changes to this project will be documented in this file.
 
-# [0.2.2] - 2025
+# [0.2.3] - 2025
+
+### Added
+
+- Files required for testing are now hosted externally and automatically downloaded when running tests.
+
+# [0.2.2] - 2025-09-24
 
 ### Added
 
--- test/defaults.py
+++ test/defaults.py
@@ -1,7 +1,12 @@
 """Default values and constants for testing the greenWTE package."""
 
+from os.path import join
+from pathlib import Path
+
 DEFAULT_TEMPERATURE = 100  # K
 DEFAULT_THERMAL_GRATING = 10**4  # rad/m
 DEFAULT_TEMPORAL_FREQUENCY = 0  # rad/s
-SI_INPUT_PATH = "test/Si-kappa-m555.hdf5"
-CSPBBR3_INPUT_PATH = "test/CsPbBr3-kappa-m443.hdf5"
+
+_base = Path(__file__).parent
+SI_INPUT_PATH = join(_base, "Si-kappa-m555.hdf5")
+CSPBBR3_INPUT_PATH = join(_base, "CsPbBr3-kappa-m443.hdf5")
